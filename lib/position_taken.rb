32215061aff2cd60793acaf977dd0be92219884e
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-02-17 18:46:10 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,7 @@
-# code your #position_taken? method here!
+def position_taken?(board, index = "X")
+  if board[index] == "" || board[index] == " "
+    false
+  else board[index] != "" || board[index] != " "
+    true
+end
+end
